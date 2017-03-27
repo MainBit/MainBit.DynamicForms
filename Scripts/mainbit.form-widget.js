2b6a59f4bacb7c39362a5e70ded9cc6b18c9ddd2
Alex Petryakov <alexpetryakov@gmail.com>
2017-03-27 22:32:58 +0300

    Start doing form widget for external sites.

---
+++ Scripts/mainbit.form-widget.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+    $('.mb-form-widget').each(function () {
+        var $form = $(this);
+        var src = $form.data('src');
+        $form.html('<iframe src="' + src + '" class="mb-form-widget-frame"></iframe>');
+    });
+});
